d4b3d35e5fea64ed75884abfcbb67222b40cf937
조다운 <38212743+jodawoooon@users.noreply.github.com>
2021-05-21 18:41:43 +0900

    store 새로고침 개선

--- happy-house/src/store/store.js
+++ happy-house/src/store/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex);
 
@@ -44,5 +45,7 @@ export default new Vuex.Store({
       return state.curPage;
     },
   },
+  modules: {},
+  plugins : [createPersistedState()],
   
 });
