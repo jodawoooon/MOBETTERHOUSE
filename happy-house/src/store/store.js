6e581a318ed9b37cb6ea8a6a0f1ca0259fa332e8
조다운 <38212743+jodawoooon@users.noreply.github.com>
2021-05-22 02:17:05 +0900

    nav bar - dropdown, login

--- happy-house/src/store/store.js
+++ happy-house/src/store/store.js
@@ -7,6 +7,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     userInfo: {
+      // isLogin: true,
       isLogin: false,
       userEmail: 'ssafy@ssafy.com',
       userPassword: '1234',
